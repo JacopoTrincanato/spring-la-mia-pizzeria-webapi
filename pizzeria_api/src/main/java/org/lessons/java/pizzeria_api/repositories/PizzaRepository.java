f41ed780aed28b7584b2ef9c5cb83b12d3d40c0e
JacopoTrincanato <trincanatoj@gmail.com>
2025-04-03 13:01:17 +0200

    implement methods in PizzaService.java and add a method for search a pizza by name in PizzaRepository.java

--- pizzeria_api/src/main/java/org/lessons/java/pizzeria_api/repositories/PizzaRepository.java
+++ pizzeria_api/src/main/java/org/lessons/java/pizzeria_api/repositories/PizzaRepository.java
@@ -1,8 +1,11 @@
 package org.lessons.java.pizzeria_api.repositories;
 
+import java.util.List;
+
 import org.lessons.java.pizzeria_api.models.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
-
+    // creo il metodo per trovare la pizza per titolo
+    public List<Pizza> findByNameContaining(String nome);
 }
--- pizzeria_api/src/main/java/org/lessons/java/pizzeria_api/services/PizzaService.java
+++ pizzeria_api/src/main/java/org/lessons/java/pizzeria_api/services/PizzaService.java
@@ -1,5 +1,10 @@
 package org.lessons.java.pizzeria_api.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.lessons.java.pizzeria_api.models.OffertaSpeciale;
+import org.lessons.java.pizzeria_api.models.Pizza;
 import org.lessons.java.pizzeria_api.repositories.OffertaSpecialeRepository;
 import org.lessons.java.pizzeria_api.repositories.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +17,47 @@ public class PizzaService {
     private PizzaRepository pizzaRepository;
 
     @Autowired
-    private OffertaSpecialeRepository offertaSpecialeRepository;
+    private OffertaSpecialeRepository offertaRepository;
+
+    // metodo per trovare tutte le pizze
+    public List<Pizza> findPizze() {
+        return pizzaRepository.findAll();
+    }
+
+    // metodo per trovare le pizze per nome
+    public List<Pizza> findPizzeByName(String nome) {
+        return pizzaRepository.findByNameContaining(nome);
+    }
+
+    // metodo per trovare la pizza per id
+    public Optional<Pizza> findPizzaById(Integer id) {
+        return pizzaRepository.findById(id);
+    }
+
+    public Pizza getPizzaById(Integer id) {
+        // gestisco la casistica in cui non venga trovata la risposta
+        Optional<Pizza> pizzaAttempt = pizzaRepository.findById(id);
+
+        return pizzaAttempt.get();
+    }
+
+    // salvataggio della pizza
+    public Pizza createPizza(Pizza pizza) {
+        return pizzaRepository.save(pizza);
+    }
+
+    // aggiornamento della pizza
+    public Pizza updatePizza(Pizza pizza) {
+        return pizzaRepository.save(pizza);
+    }
+
+    // cancellazione della pizza
+    public void deletePizza(Pizza pizza) {
+        for (OffertaSpeciale offerteDaEliminare : pizza.getOfferteSpeciali()) {
+            offertaRepository.delete(offerteDaEliminare);
+        }
+
+        // cancello la pizza
+        pizzaRepository.delete(pizza);
+    }
 }
